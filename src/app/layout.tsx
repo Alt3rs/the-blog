5b44501bb5d2135a454ecb89ad2970a6a0aa89a3
Pedro Rios <prios731@gmail.com>
2025-05-16 15:43:50 -0300

    Creating a PostModel to model the post.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,26 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "The  Blog",
-  description: "Essa seria a descrição  dessa  página.",
+    title: "The  Blog",
+    description: "Essa seria a descrição  dessa  página.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt-BR">
-      <body>
-        <header>
-          <h1>Header</h1>
-        </header>
-        {children}
-        <footer>
-          <h1>Footer</h1>
-        </footer>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt-BR">
+            <body>{children}</body>
+        </html>
+    );
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import clsx from "clsx";
+
 export default function HomePage() {
-  return <h1>Hello World from / page.tsx</h1>;
+    return (
+        <h1
+            className={clsx(
+                "text-2xl",
+                "font-bold",
+                "text-blue-500",
+                "hover:text-blue-400",
+                "hover:bg-blue-200",
+                "transition",
+                "duration-300"
+            )}
+        >
+            Hello World from / page.tsx
+        </h1>
+    );
 }
