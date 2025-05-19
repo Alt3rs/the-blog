1c775e5528b881ae62dc4960b112eac6ac2dcc02
Pedro Rios <prios731@gmail.com>
2025-05-19 11:45:13 -0300

    Suspend to display fallback when something is loading.

--- src/components/SpinLoader/index.tsx
+++ src/components/SpinLoader/index.tsx
@@ -1,23 +1,18 @@
 import clsx from 'clsx';
 
 type SpinLoaderProps = {
-  containerClasses?: string;
+  className?: string;
 };
 
-export function SpinLoader({ containerClasses = '' }: SpinLoaderProps) {
-  const classes = clsx(
-    'flex',
-    'items-center',
-    'justify-center',
-    containerClasses,
-  );
+export function SpinLoader({ className = '' }: SpinLoaderProps) {
+  const classes = clsx('flex', 'items-center', 'justify-center', className);
 
   return (
     <div className={classes}>
       <div
         className={clsx(
           'w-10 h-10',
-          'border-5 border-t-transparent border-slate-900',
+          'border-5 border-t-transparent border-slate-500',
           'rounded-full',
           'animate-spin',
         )}
