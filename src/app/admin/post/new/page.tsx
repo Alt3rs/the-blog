3332bb55f989dc0aa402b031af6eeab5d11accd5
Pedro Rios <prios731@gmail.com>
2025-05-29 11:44:18 -0300

    Using TypeScript to add style variations to the component.

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,8 +4,16 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16'>
-      <Button type='submit'>Funciona ocmo o do JSX</Button>
+    <div className='py-16 flex flex-wrap gap-4'>
+      <Button variant='default' type='submit'>
+        Confirma
+      </Button>
+      <Button variant='ghost' type='submit'>
+        Confirmar
+      </Button>
+      <Button variant='danger' type='submit'>
+        Confirma
+      </Button>
     </div>
   );
 }
